8d06b096218b67d19e0d72f7c312ea77b3a2c467
Sumit Bhardwaj <62645978+Bhardwaz@users.noreply.github.com>
2026-04-15 12:02:32 +0530

    Update signin.js

--- src/controllers/auth/signin.js
+++ src/controllers/auth/signin.js
@@ -17,7 +17,7 @@ async function signin(req, res){
            res.cookie("token", token, {
              httpOnly:true,
              secure:true,
-             sameSite:"lax",
+             sameSite:"none",
              domain: ".datekarle.com",
              expires: new Date(Date.now() + 48 * 3600000),
            })
